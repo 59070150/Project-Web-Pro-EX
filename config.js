75f5e1a7949cee94d6061d0d84fd24cb037d8ec4
Wachirawit <42531665+59070150@users.noreply.github.com>
2021-06-23 13:55:54 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -4,7 +4,13 @@ const connection = mysql.createConnection({
 	host     : 'localhost',
 	user     : 'root',
 	password : '12345678',
-	database : 'semeter_db'
+	database : 'semeter_db',
+	pool: {
+		max: 5,
+		min: 0,
+		acquire: 30000,
+		idle: 10000
+	  }
 });
 
 module.exports = connection;
